8ad4dfb1488237948d7a653eb26a3f0db57bb281
Yash Tiwari <yasht.2701@gmail.com>
2022-12-15 11:58:07 +0530

    fix: change to BasicCard view

--- lib/screens/onboarding/widgets/foreground_view.dart
+++ lib/screens/onboarding/widgets/foreground_view.dart
@@ -45,7 +45,7 @@ class CardsView extends StatelessWidget {
       Positioned(
         left: kPositionMap[onboardingData]![0],
         top: kPositionMap[onboardingData]![1],
-        child: FamilyCard(size: size),
+        child: BasicCard(size: size),
       ),
     ];
 
